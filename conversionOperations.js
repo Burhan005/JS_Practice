f6ecc001c24ea249dfdecff5cce4e7a4274865a1
Burhan Kapasi <burhankapasi@Burhans-MacBook-Air.local>
2025-04-07 10:47:16 +0530

    Added conversionOperations.js file

---
+++ conversionOperations.js
@@ -0,0 +1,15 @@
+// let score=33;
+
+// console.log(typeof score);
+
+// let valueInNumber=Number(score)
+
+// let isLoggedin=1
+// let booleanIsLoggedIn= Boolean(isLoggedin)
+// console.log(booleanIsLoggedIn); // true
+
+let num=33;
+let stringNum=String(num)
+console.log(stringNum);
+console.log(typeof stringNum);
+
