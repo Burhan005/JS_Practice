877af0fda956b21f0762cccb156a544786d96313
Burhan Kapasi <burhankapasi@Burhans-MacBook-Air.local>
2025-04-07 10:30:32 +0530

    Added dataType.js file

---
+++ dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";// treat all js code as newer version
+
+// alert("hello")
+
+let name ="burhan"; // string
+let age=18; //  number
+
+let isLoggedin=false;
+
+// console.log(typeof "burhan")
+console.log(typeof null);
+
