2b9242988aaf0003161663df6141a4e85a3c2050
Burhan Kapasi <burhankapasi@Burhans-MacBook-Air.local>
2025-04-07 10:07:18 +0530

    Updated logic in app.js

---
+++ variables.js
@@ -0,0 +1,16 @@
+const accountId=144553
+let accountEmail="burhan@demo.com";
+var accountPassword="12345";
+let accountState;//     
+
+accountCity="jaipur";
+
+// accountId=2; error
+
+accountEmail="acc@demo.com";
+accountPassword="1234";
+accountCity="mumbai";
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
