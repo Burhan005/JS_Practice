ec55d6b3de651bec7b293d89c39663200cb727ba
Burhan Kapasi <burhankapasi@Burhans-MacBook-Air.local>
2025-04-08 09:22:29 +0530

    summary

---
+++ dataTypesSummary.js
@@ -0,0 +1,14 @@
+// primitive data type 
+// ==================
+// String : type of string
+// Number : type of number 
+// BigInt : type of Bigint
+// Boolean : type of boolean
+// Null : type of object *
+// Undefined : type of undefined 
+// Symbol : type of Symbol
+
+// reference data type 
+// ==================
+// array , object , function 
+// only fn type of function and other object 
