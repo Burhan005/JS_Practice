0f032164e092f013c30a89bd2283b317dc5e0d46
Burhan Kapasi <burhankapasi@Burhans-MacBook-Air.local>
2025-04-08 08:50:22 +0530

    conversion.js

--- conversionOperations.js
+++ conversionOperations.js
@@ -10,7 +10,28 @@
 
 let num=33;
 let stringNum=String(num)
-console.log(stringNum);
-console.log(typeof stringNum);
+// console.log(stringNum);
+// console.log(typeof stringNum);
+
+// operation
+
+let value = 3
+let negValue=-value
+// console.log(negValue)
+
+let str1 ="hello"
+let str2=" welcome"
+let str3= str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2")
+
+// console.log("1"+2+2)
+
+// console.log(1+2+"2") 
+// console.log(+true)
+// console.log(+"")
+
 
 
